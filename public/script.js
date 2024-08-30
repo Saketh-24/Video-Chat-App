310c189cb4951e1bed6ace113269b398ad903365
saketh-24 <saketh1094@gmail.com>
2024-08-30 22:28:34 +0530

    added PeerJS

--- public/script.js
+++ public/script.js
@@ -1,14 +1,38 @@
 // Connect to the Socket.IO server running on localhost:4000
 const socket = io("http://localhost:4000");
+const videoGrid = document.getElementById("video-grid");
+var myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
 
-socket.on("connect", () => {
-  console.log("Connected to server");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
 
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+
+    // Listen for the 'user-connected' event from the server
+    socket.on("user-connected", (userId) => {
+      // console.log("User connected:", userId);
+      connectToNewUser(userId, stream);
+    });
+  });
+
+myPeer.on("open", (id) => {
   // Emit the 'join-room' event with room ID and user ID
-  socket.emit("join-room", ROOM_ID, 20);
+  socket.emit("join-room", ROOM_ID, id);
 });
 
-// Listen for the 'user-connected' event from the server
-socket.on("user-connected", (userId) => {
-  console.log("User connected:", userId);
-});
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
